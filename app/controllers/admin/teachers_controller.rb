1ea56193d924dc165e27da9d5040535e94e34832
Alex Boyarinov <a.boyarinov@.gmail.com>
2020-02-06 15:06:32 +0400

    добавление формы Преподаватели + стили

--- app/controllers/admin/teachers_controller.rb
+++ app/controllers/admin/teachers_controller.rb
@@ -4,9 +4,18 @@ class Admin::TeachersController < Admin::BaseController
   end
 
   def new
+    @teacher = Teacher.new
   end
 
   def create
+    @teacher = Teacher.new(teacher_params)
+
+    if @teacher.save
+        redirect_to admin_teachers_path, notice: 'Преподаватель успешно создан'
+    else
+      flash.now[:alert] = 'Не удалось создать преподавателя'
+      render :new
+    end
   end
 
   def edit
@@ -20,5 +29,8 @@ class Admin::TeachersController < Admin::BaseController
   private
   def set_active_main_menu_item
     @main_menu[:teachers][:active] = true
+  end
+  def teacher_params
+    params.require(:teacher).permit(:first_name, :middle_name, :last_name, :description)
   end
 end
--- app/models/teacher.rb
+++ app/models/teacher.rb
@@ -1,2 +1,6 @@
 class Teacher < ApplicationRecord
+  validates :first_name, presence: true
+  validates :middle_name, presence: true
+  validates :last_name, presence: true
+  validates :description, presence: true
 end
